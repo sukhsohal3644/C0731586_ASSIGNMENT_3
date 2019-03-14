97916f8e3f930c412b047f1e8cfd1ede2455750e
visualstudio <visualstudio@230-PC30>
2019-03-14 15:01:06 -0400

    nhj

---
+++ BankTests/BankAccountTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BankTests
+{
+    [TestClass]
+    public class BankAccountTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
